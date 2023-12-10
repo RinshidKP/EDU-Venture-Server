fef56b9400df678eec6ed0181daf884f597ce7ec
RinshidKP <rinshid26@gmail.com>
2023-12-10 19:47:15 +0530

    updated CORS policy again again

--- server.mjs
+++ server.mjs
@@ -23,11 +23,10 @@ cors: {
 
 export const userSockets = {}
 
-const corsOptions ={
+const corsOptions = {
   origin: [
-    'eduventure-445wgs6if-rinshids-projects.vercel.app',
     'http://eduventure-445wgs6if-rinshids-projects.vercel.app',
-    'https://eduventure-445wgs6if-rinshids-projects.vercel.app'
+    'https://eduventure-445wgs6if-rinshids-projects.vercel.app',
   ],
   methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
   credentials: true,
@@ -36,12 +35,7 @@ const corsOptions ={
 
 app.use(cookieParser());
 dotenv.config();
-app.use(cors({
-  "origin": "*",
-  "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
-  "preflightContinue": false,
-  "optionsSuccessStatus": 204
-}));
+app.use(cors(corsOptions));
 app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({extended:true},{limit: '50mb'}));
 app.use(express.static("public"));
