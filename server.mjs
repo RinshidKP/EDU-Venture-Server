86948412f3043fb097b785c5ed2d4359aafb9f9f
RinshidKP <rinshid26@gmail.com>
2023-12-10 19:36:02 +0530

    updated CORS policy again

--- server.mjs
+++ server.mjs
@@ -36,7 +36,12 @@ const corsOptions ={
 
 app.use(cookieParser());
 dotenv.config();
-app.use(cors(corsOptions));
+app.use(cors({
+  "origin": "*",
+  "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+  "preflightContinue": false,
+  "optionsSuccessStatus": 204
+}));
 app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({extended:true},{limit: '50mb'}));
 app.use(express.static("public"));
