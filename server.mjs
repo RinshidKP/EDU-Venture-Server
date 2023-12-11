be47d0b1ce093e816049e4ed8cdf6fd3dc5ae148
RinshidKP <rinshid26@gmail.com>
2023-12-11 11:33:48 +0530

    updated cors again 1

--- server.mjs
+++ server.mjs
@@ -20,7 +20,6 @@ const corsOptions = {
     'http://edu-venture-client.vercel.app',
     'https://edu-venture-client.vercel.app',
   ],
-  methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
   credentials: true,
 };
 
